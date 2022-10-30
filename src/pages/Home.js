0091929762f1d872663abf875d6c74ca15746b18
JamyPut <jp2001dj@gmail.com>
2022-10-30 23:01:23 +0100

    created diagonal separtor on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,21 @@ import {NavLink} from "react-router-dom";
 function Home() {
 
     return(
-        <div className={"home-container"}>
-        <h1 className={"nemo-title"}>
-            Welkom bij Nemo.
-            <p>Wij helpen je navigeren in een zee vol cocktails!</p>
-        </h1>
-            <button className={"cocktail-button"}>
-               <NavLink to={"/cocktails"}>Alle cocktails</NavLink>
-            </button>
-        </div>
+        <>
+            <div className={"home-container"}>
+                <h1 className={"nemo-title"}>
+                    Welkom bij Nemo.
+                    <p>Wij helpen je navigeren in een zee vol cocktails!</p>
+                </h1>
+                <button className={"cocktail-button"}>
+                    <NavLink to={"/cocktails"}>Alle cocktails</NavLink>
+                </button>
+            </div>
+            <div className={"diagonal-separator"}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aspernatur at consectetur ducimus earum eum eveniet facilis impedit incidunt iusto nam natus neque odit reprehenderit, tenetur ut veritatis. Accusantium, animi atque commodi earum itaque labore non obcaecati </p>
+                <button>verras me</button>
+            </div>
+        </>
     );
 }
 
