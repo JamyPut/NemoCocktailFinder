dcdb9b9e8d0c337ca3b727a044d9ede27f65d1fe
JamyPut <jp2001dj@gmail.com>
2022-11-10 00:08:31 +0100

    finished home page and implemented searchbox

--- src/pages/Cocktails.js
+++ src/pages/Cocktails.js
@@ -23,18 +23,11 @@ function Cocktails(){
         console.log(url)
     };
 
-    // const getSearchInput = () => {
-    //     const searchUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputRef}`
-    //
-    //     setUrl(searchUrl);
-    //     console.log(url)
-    //     console.log(searchUrl)
-    // };
     const getSearchInput = (event)=>{
         event.preventDefault();
-        const searchUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputRef}`
+        console.log(inputRef)
+        const searchUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputRef.current.value}`
         setUrl(searchUrl);
-        // show the user input value to console
         console.log(url);
     };
 
@@ -52,14 +45,6 @@ function Cocktails(){
             .catch(e => console.log(e))
     }
 
-    // const searchCocktail = () => {
-    //     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputRef}`)
-    //         .then(res => {
-    //             console.log(res.data)
-    //             setCocktails(res.data.drinks)
-    //         })
-    //         .catch(e => console.log(e))
-    // }
 
 
     //        load cocktail upon page load.
@@ -67,23 +52,27 @@ function Cocktails(){
         fetchCocktail();
     },[url]);
 
-    // if (cocktails.length < 1){
-    //     return (
-    //         <div className={"cocktail-page-styling"}>
-    //             <div className={"no-results-styling"}>
-    //                 <h1 className='section-title'>
-    //                 Vind jou cocktails!
-    //                 probeer een van de filter knoppen of vul een nieuw zoekresultaat in.
-    //             </h1>
-    //                 <section className={"cocktail-filters"}>
-    //                     <button onClick={alcoholClick}>met alcohol</button>
-    //                     <button onClick={nonAlcoholClick}>zonder alcohol</button>
-    //                     <input type="text" ref={inputRef} onChange={getSearchInput}/>
-    //                 </section>
-    //             </div>
-    //         </div>
-    //     )
-    // }
+
+    if (cocktails && cocktails.length < 1){
+        return (
+            <div className={"cocktail-page-styling"}>
+                <div className={"no-results-styling"}>
+                    <h1 className='section-title'>
+                    Vind jou cocktails!
+                    probeer een van de filter knoppen of vul een nieuw zoekresultaat in.
+                </h1>
+                    <section className={"cocktail-filters"}>
+                        <button onClick={alcoholClick}>met alcohol</button>
+                        <button onClick={nonAlcoholClick}>zonder alcohol</button>
+                        <form onSubmit={getSearchInput}>
+                            <input placeholder={"bijv: pina colada"} type="text" ref={inputRef}/>
+                            <button id={"submit-button"} type={"submit"}>zoek</button>
+                        </form>
+                    </section>
+                </div>
+            </div>
+        )
+    }
     return(
         <div className={"cocktail-page-styling"}>
             <section className={"cocktail-filters"}>
@@ -91,13 +80,13 @@ function Cocktails(){
                 <button onClick={nonAlcoholClick}>zonder alcohol</button>
                 <form onSubmit={getSearchInput}>
                     <input type="text" ref={inputRef}/>
-                    <button type={"submit"}> zoek</button>
+                    <button placeholder={"bijv: pina colada"} id={"submit-button"} type={"submit"}>zoek</button>
                 </form>
             </section>
             <section className='section'>
                 <h1 className='section-title'>Cocktails</h1>
                 <div className='cocktail-list'>
-                    {cocktails.map(({ strDrink, strDrinkThumb, idDrink}) => {
+                    {cocktails && cocktails.map(({ strDrink, strDrinkThumb, idDrink}) => {
                         return <SingleCocktailComponent key={idDrink} id={idDrink} image={strDrinkThumb} name={strDrink}  />;
                     })}
                 </div>
