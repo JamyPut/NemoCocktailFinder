9fc0f8d63980a4a1e5ac1c08462df7d15c719b92
JamyPut <jp2001dj@gmail.com>
2022-11-11 22:14:43 +0100

    worked on registration form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from "react";
 import "./Register.css"
-import {useState} from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
-function Register(){
+const usernameREGEX = /.{6,24}/;
+const passREGEX = /.{6,24}/;
+const emailREGEX = /@/;
+
+const Register = () =>{
+
+    const [username, setUsername] = useState("");
+    const [pass, setPass] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+    const [email, setEmail] = useState("");
+
+    const [validUsername, setValidUsername] = useState(false);
+    const [validPass, setValidPass] = useState(false);
+    const [validConfirmedPass, setValidConfirmedPass] = useState(false)
+    const [validEmail, setValidEmail] = useState(false)
+
+    const [errorMessage, setErrorMessage] = useState("")
+    const [succes, setSucces] = useState("")
+
+    useEffect(() => {
+        const result = usernameREGEX.test(username);
+        console.log(result);
+        console.log(username);
+        setValidUsername(result);
+    }, [username])
+
+    useEffect(() => {
+        const result = passREGEX.test(pass);
+        console.log(result);
+        console.log(pass);
+        setValidPass(result);
+        const checkConfirmedPass = pass === confirmPass;
+        setValidConfirmedPass(checkConfirmedPass);
+    }, [pass, confirmPass])
+
+    useEffect(() => {
+        setErrorMessage("");
+    }, [username, pass, confirmPass])
+
+
+// function Register(){
 
     const [fetchedData, setFetcheddata] = useState()
     const [url, seturl] = useState()
@@ -19,11 +59,56 @@ function Register(){
             .catch(e => console.log(e));
     }
     return(
-        <div className={"login-page-styling"}>
-            <h1>Dit is de register pagina</h1>
-            <button onClick={testServer}>klik hier</button>
-            <p>api informatie: {fetchedData}</p>
+        <div  className={"register-page-styling"}>
+            <section>
+                <h1>Dit is de register pagina</h1>
+                <button onClick={testServer}>klik hier</button>
+                <p>api informatie: {fetchedData}</p>
+            </section>
+            <section className={"register-form-section"}>
+                <form>
+                    <h3>Vul de gegevens in</h3>
+                    <hr className={"rounded"}/>
+                    <label htmlFor="username">Gebruikersnaam:</label>
+                    <input
+                        placeholder={"Naam"}
+                        type="text"
+                        id={"username"}
+                        autoComplete={"off"}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        placeholder={"Email"}
+                        type="email"
+                        id={"email"}
+                        autoComplete={"off"}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="pass">Wachtwoord:</label>
+                    <input
+                        placeholder={"********"}
+                        type="password"
+                        id={"pass"}
+                        onChange={(e) => setPass(e.target.value)}
+                    />
+                    <label htmlFor="confirmPass">Bevestig wachtwoord:</label>
+                    <input
+                        placeholder={"********"}
+                        type="password"
+                        id={"confirmPass"}
+                        onChange={(e) => setConfirmPass(e.target.value)}
+                    />
+                    <button disabled={!validUsername || !validEmail || !validPass || !validConfirmedPass}>
+                        Inloggen
+                    </button>
+                </form>
+                <hr className={"rounded"}/>
+                <Link to={"/login"}>Heb je al een account? klik hier om in te loggen</Link>
+            </section>
         </div>
+
     );
 }
 export default Register;
