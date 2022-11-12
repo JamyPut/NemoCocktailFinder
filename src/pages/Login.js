ff2b52e61a00c29c51cbf2bc07b250b4bee7c1f8
JamyPut <jp2001dj@gmail.com>
2022-11-12 14:25:08 +0100

    login is now functional and created authcontext

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
+import AuthContext from "../context/AuthProvider";
 import "./Login.css"
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
 
+    const {setAuth} = useContext(AuthContext)
     const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("")
     const [pass, setPass] = useState("");
     const [errorMessage, setErrorMessage] = useState("")
 
@@ -17,16 +20,36 @@ const Login = () => {
 
     const loginUser = async (e) => {
         e.preventDefault();
-        console.log(username, pass);
-        setUsername("");
-        setPass("");
 
+        try {
+            const response = await axios.post(serverUrl, JSON.stringify({username, password: pass}),
+                {
+                    headers: {"Content-Type": "application/json" },
+                }
+                );
+            console.log(response.data)
+            const accesToken = response?.data?.accesToken;
+            const roles = response?.data?.roles;
+            setEmail(response.data.email)
+            setAuth({username, pass, roles, accesToken});
+            console.log(username, pass);
+            setUsername("");
+            setPass("");
+        } catch (error){
+            if (!error?.response){
+                setErrorMessage("No response from server");
+            } else {
+                console.log(error.response)
+                setErrorMessage(error.response.data.message)
+            }
+        }
     }
 
     return(
         <div className={"login-page-styling"}>
             <h1>Dit is de login pagina</h1>
             <section className={"login-form-section"}>
+                <p>Ingelod met {email}</p>
                 <form onSubmit={loginUser}>
                     <h3>Vul je inloggegevens in</h3>
                     <hr className={"rounded"}/>
