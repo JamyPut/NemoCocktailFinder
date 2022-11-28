e0538fe2b01fca1a64904e6ac2afedb6791cd42e
JamyPut <jp2001dj@gmail.com>
2022-11-28 23:12:58 +0100

    added faq questions

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -6,22 +6,30 @@ import FaQuestion from "../components/FaqSection/FaQuestion";
 function Faq() {
     return (
         <div className={"faq-page-styling"}>
-            <h1>Veelgestelde vragen</h1>
+            <h1 id={"faq-title"}>Veelgestelde vragen</h1>
             <section className={"question-section"}>
                 <FaQuestion
                     title={"Hoe sla ik mijn favorieten lijst op?"}
-                    answer={"op de pagina waar de favorieten lijst is weergegeven, " +
-                        "klik je op de rechtermuisknop -> print -> opslaan als pdf"}
+                    answer={"Op de pagina waar de favorieten lijst is weergegeven, " +
+                        "klik je met de rechtermuisknop op een lege plek -> print -> opslaan als pdf."}
                 />
                 <FaQuestion
                     title={"Hoe voeg ik een cocktail toe aan favorieten?"}
-                    answer={"Klik op het ster icoon dat zich bij de betreffende cocktail bevind." +
+                    answer={"Klik op het ster icoon dat zich bij de betreffende cocktail bevindt. " +
                         "Je cocktail staat nu bij favorieten!"}
                 />
                 <FaQuestion
                     title={"Kan ik mijn lijsten bewaren voor een later bezoek aan de website?"}
-                    answer={"Momenteel is het niet mogelijk lijsten te bewaren." +
-                        "De lijst zal worden gewist zodra de pagina gesloten word."}
+                    answer={"Momenteel is het niet mogelijk lijsten te bewaren. " +
+                        "De lijst blijft bewaard totdat het tabblad gesloten word."}
+                />
+                <FaQuestion
+                    title={"Waar kan ik mijn favorieten vinden?"}
+                    answer={"De lijst met favorieten is te vinden op de profiel pagina."}
+                />
+                <FaQuestion
+                    title={"Hoe kan ik mij registreren?"}
+                    answer={"Als je op inloggen klikt verschijnt ook de optie om je te registreren."}
                 />
             </section>
             <section>
