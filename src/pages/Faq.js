ca50e2028fe93d1d8ab15686b07df306e6b0bd53
JamyPut <jp2001dj@gmail.com>
2022-11-28 23:57:13 +0100

    updated readme

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -14,9 +14,9 @@ function Faq() {
                         "klik je met de rechtermuisknop op een lege plek -> print -> opslaan als pdf."}
                 />
                 <FaQuestion
-                    title={"Hoe voeg ik een cocktail toe aan favorieten?"}
-                    answer={"Klik op het ster icoon dat zich bij de betreffende cocktail bevindt. " +
-                        "Je cocktail staat nu bij favorieten!"}
+                    title={"Hoe pas ik mijn favorieten lijst aan?"}
+                    answer={"Klik op favoriet toevoegen om de cocktail aan favorieten toe te voegen." +
+                        "klik op verwijderen om de cocktail uit favorieten te verwijderen."}
                 />
                 <FaQuestion
                     title={"Kan ik mijn lijsten bewaren voor een later bezoek aan de website?"}
@@ -24,7 +24,8 @@ function Faq() {
                 />
                 <FaQuestion
                     title={"Waar kan ik mijn favorieten vinden?"}
-                    answer={"De lijst met favorieten is te vinden op de profiel pagina."}
+                    answer={"De lijst met favorieten is te vinden op de profiel pagina." +
+                        "klik op het refresh icoon dat zich naast de favorieten titel bevind om je favorieten op te halen/bij te werken"}
                 />
                 <FaQuestion
                     title={"Hoe kan ik mij registreren?"}
